dcc0898bcbede2faa2a31febbc009be63bafa34a
Kirubel2022 <104833561+Kirubel2022@users.noreply.github.com>
2022-07-19 16:09:52 -0700

    Update CourseServiceTest.java

--- CourseServiceTest.java
+++ CourseServiceTest.java
@@ -1,4 +1,13 @@
 package test;
+import com.generation.service.CourseService;
+import org.junit.jupiter.api.test;
+
+import static org.junit.junit.jupiter.api.Assertions.assertEquals;
 
 public class CourseServiceTest {
+    @Test
+    void courseCodeOfINTRO_CS_1(){
+        courseService courseService= new CourseService();
+        assertEquals(expected:"INTRO-WEB-1",courseService.getCourse(code:"INTRO-WEB-1").getcode());
+    }
 }
